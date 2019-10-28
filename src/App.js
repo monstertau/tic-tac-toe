921ca56d57f8e1465615c5830eb40523d416e9f9
Trung Dung Vu <trungdungvu172@gmail.com>
2019-10-28 10:55:46 +0700

    Done minimax prototype

--- src/App.js
+++ src/App.js
@@ -2,43 +2,49 @@ import React from "react";
 import "./App.css";
 import Game from "./Game/Game";
 class App extends React.Component {
-  state = {
-    turn: "X",
-    gameEnded: false,
-    result: "",
-    board: Array(9).fill("")
-  };
-  game = new Game();
-
-  reset = event => {
-    this.setState({
+  constructor(props) {
+    super(props);
+    this.board = Array(9).fill("");
+    this.state = {
       turn: "X",
       gameEnded: false,
       result: "",
       board: Array(9).fill("")
+    };
+  }
+  game = new Game();
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      this.state.turn != prevState.turn ||
+      this.state.result != prevState.result
+    ) {
+      this.checkWinner();
+    }
+  }
+  reset = event => {
+    this.setState({
+      turn: "X",
+      gameEnded: false,
+      result: ""
     });
+    this.board = Array(9).fill("");
   };
 
   clicked = event => {
     console.log(this.state);
+
     const dataSquare = event.target.dataset.square;
-    const tempBoard = [...this.state.board];
-    tempBoard[dataSquare] = this.state.turn;
-    if (this.state.board[dataSquare] === "" && !this.state.gameEnded) {
-      this.setState(
-        {
-          turn: this.state.turn === "X" ? "O" : "X",
-          board: tempBoard
-        },
-        function() {
-          this.checkWinner();
-        }
-      );
+    if (this.board[dataSquare] === "" && !this.state.gameEnded) {
+      this.setState({
+        turn: this.state.turn === "X" ? "O" : "X"
+      });
+      this.board[dataSquare] = this.state.turn;
     }
   };
 
   checkWinner = () => {
-    const result = this.game.isEnd(this.state.board);
+    console.log(this.board);
+    const result = this.game.isEnd(this.board);
     if (result == "X" || result == "O") {
       this.setState({
         result: "The winner is " + result,
@@ -49,6 +55,17 @@ class App extends React.Component {
         result: "The game is draw",
         gameEnded: true
       });
+    }
+    if (this.state.turn == "X") {
+      let [m, pos1] = this.game.min(this.board);
+      console.log("x value: " + m + " pos: " + pos1);
+    } else if (this.state.turn == "O") {
+      let [m, pos2] = this.game.max(this.board);
+      this.setState({
+        turn: "X"
+      });
+      this.board[pos2] = "O";
+      console.log("o value: " + m + " pos: " + pos2);
     }
   };
 
@@ -61,31 +78,31 @@ class App extends React.Component {
           onClick={event => this.clicked(event)}
         >
           <div className="col-3 tic " data-square="0">
-            {this.state.board[0]}
+            {this.board[0]}
           </div>
           <div className="col-3 tic " data-square="1">
-            {this.state.board[1]}
+            {this.board[1]}
           </div>
           <div className="col-3 tic " data-square="2">
-            {this.state.board[2]}
+            {this.board[2]}
           </div>
           <div className="col-3 tic" data-square="3">
-            {this.state.board[3]}
+            {this.board[3]}
           </div>
           <div className="col-3 tic " data-square="4">
-            {this.state.board[4]}
+            {this.board[4]}
           </div>
           <div className="col-3 tic " data-square="5">
-            {this.state.board[5]}
+            {this.board[5]}
           </div>
           <div className="col-3 tic " data-square="6">
-            {this.state.board[6]}
+            {this.board[6]}
           </div>
           <div className="col-3 tic " data-square="7">
-            {this.state.board[7]}
+            {this.board[7]}
           </div>
           <div className="col-3 tic " data-square="8">
-            {this.state.board[8]}
+            {this.board[8]}
           </div>
         </div>
         <button
@@ -95,7 +112,11 @@ class App extends React.Component {
         >
           Reset Game
         </button>
-        {this.state.gameEnded ? <div>{this.state.result}</div> : <div>Its {this.state.turn} Turn</div>}
+        {this.state.gameEnded ? (
+          <div>{this.state.result}</div>
+        ) : (
+          <div>Its {this.state.turn} Turn</div>
+        )}
       </div>
     );
   }
--- src/Game/Game.jsx
+++ src/Game/Game.jsx
@@ -1,12 +1,63 @@
-import React from 'react'
+import React from "react";
 class Game extends React.Component {
-  isEnd = (board) => {
+  max = board => {
+    let maxv = -2;
+    let pos;
+    let result = this.isEnd(board);
+    // console.log(result);
+    if (result === "X") {
+      return [-1, 0];
+    } else if (result === "O") {
+      return [1, 0];
+    } else if (result === ".") {
+      return [0, 0];
+    }
+    for (let i = 0; i < 9; i++) {
+      if (board[i] === "") {
+        board[i] = "O";
+        let [m, mini] = this.min(board);
+        if (m > maxv) {
+          maxv = m;
+          pos = i;
+          
+        }
+        board[i] = "";
+      }
+    }
+    return [maxv, pos];
+  };
+  min = board => {
+    let minv = 2;
+    let pos;
+    const result = this.isEnd(board);
+    if (result === "X") {
+      return [-1, 0];
+    } else if (result === "O") {
+      return [1, 0];
+    } else if (result === ".") {
+      return [0, 0];
+    }
+    for (let i = 0; i < 9; i++) {
+      if (board[i] === "") {
+        board[i] = "X";
+        let [m, maxi] = this.max(board);
+        if (m < minv) {
+          minv = m;
+          pos = i;
+        }
+        board[i] = "";
+      }
+    }
+    return [minv, pos];
+  };
+
+  isEnd = board => {
     // vertical win
     for (let i = 0; i < 3; i++) {
       if (
-        board[0 + i] != "" &&
-        board[0 + i] == board[3 + i] &&
-        board[3 + i] == board[6 + i]
+        board[0 + i] !== "" &&
+        board[0 + i] === board[3 + i] &&
+        board[3 + i] === board[6 + i]
       ) {
         return board[0 + i];
       }
@@ -14,28 +65,28 @@ class Game extends React.Component {
     // horizontal win
     for (let i = 0; i <= 6; i += 3) {
       if (
-        board[0 + i] != "" &&
-        board[0 + i] == board[1 + i] &&
-        board[1 + i] == board[2 + i]
+        board[0 + i] !== "" &&
+        board[0 + i] === board[1 + i] &&
+        board[1 + i] === board[2 + i]
       ) {
         return board[0 + i];
       }
     }
     // main diagonal win
-    if (board[0] == board[4] && board[4] == board[8]) {
+    if (board[0] === board[4] && board[4] === board[8]) {
       return board[0];
     }
     // second diagonal win
-    if (board[2] == board[4] && board[4] == board[6]) {
+    if (board[2] === board[4] && board[4] === board[6]) {
       return board[2];
     }
     // is whole board full?
     for (let i = 0; i < 9; i++) {
       // There's an empty field, we continue the game
-      if (board[i] == "") return 0;
+      if (board[i] === "") return 0;
     }
     // it's a tie!
-    return '.'
+    return ".";
   };
 }
 export default Game;
